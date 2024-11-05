a48732d41d1e7aecf50343fa016a141027883520
Chrystalio <chrystalio.k@outlook.com>
2024-11-05 22:46:56 +0700

    add  endpoint to serve province data

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import kodewilayah from './data/kodewilayah.json';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/v1/api/provinsi', (req, res) => {
+  const provinsi = kodewilayah.map((region) => ({
+    code: region.code,
+    name: region.name,
+    level: region.level
+  }));
+  res.json(provinsi);
 });
 
 export default app;
